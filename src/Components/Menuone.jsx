94926cb7124cd9efa21a673cf575a087fabcdb13
Aditya Sharma <83112843+ad850@users.noreply.github.com>
2022-02-09 12:52:57 +0530

    Update Menuone.jsx

--- src/Components/Menuone.jsx
+++ src/Components/Menuone.jsx
@@ -4,16 +4,11 @@ import Menu from './Menu';
 
 const Menuone = () => {
     return (
-
-
         <div id="main">
             {
                 Api.map((val) => <Menu key={val.id} name={val.name} description={val.description} image={val.image} price={val.price} />)
             }
         </div>
-
-
     )
 }
-
 export default Menuone;
