cb1f512f1a9733b473a7fa4ccd28660e3d5390ff
Aditya Sharma <83112843+ad850@users.noreply.github.com>
2022-02-09 12:52:30 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-
 const Navbar = () => {
     return (
-
-
         <div id="contain">
-        
-
             <div id="nav">
             <span id="logo"/>
-                
                 <NavLink exect to="/" activeClassName="One" className="link1">Menu</NavLink>
                 <NavLink to="/Location" activeClassName="One" className="link1">Reviews</NavLink>
                 <NavLink to="/Reservation" activeClassName="One" className="link1">Reservation</NavLink>
                 <NavLink to="/About" activeClassName="One" className="link1">AboutUs</NavLink>
-
-
             </div>
-
-
         </div>
     )
 }
