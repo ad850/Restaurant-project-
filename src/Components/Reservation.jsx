6466a9f8995fc9af4f0574c7417ecc0636e8835c
Aditya Sharma <83112843+ad850@users.noreply.github.com>
2022-02-09 12:54:08 +0530

    Update Reservation.jsx

--- src/Components/Reservation.jsx
+++ src/Components/Reservation.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
 
 const onsubmit = (event) => {
-
     event.preventDefault();
-
     alert("form is submmited");
 }
 
 const Reservation = () => {
-
     return (
         <div id="rs">
-
-
             <form onSubmit={onsubmit}>
                 <div className="rcenter">
                     <input type="text" placeholder="Enter name" name="fname" className="inp" />
@@ -20,14 +15,9 @@ const Reservation = () => {
                     <input type="email" placeholder="Enter email" name="email"  className="inp"/>
                     <input type="datetime-local" id="birthdaytime" name="birthdaytime" className="inp"/>
                     <input type="submit" ></input>
-
                 </div>
             </form>
-
         </div>
-
-
     )
 }
-
 export default Reservation
